b6c06ce49f099964fc763207599651e111fa8936
martijnvandermarel <martijn.vander.marel@hetnet.nl>
2019-06-08 16:55:36 +0200

    adapt tests based on new functionality

--- march_hardware/test/TestPDOmap.cpp
+++ march_hardware/test/TestPDOmap.cpp
@@ -14,30 +14,34 @@ protected:
 TEST_F(PDOTest, sortPDOmap)
 {
   march4cpp::PDOmap pdoMapMISO = march4cpp::PDOmap();
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::StatusWord);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::ActualPosition);
+
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::ActualPosition, 1);
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::StatusWord, 3);
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::ControlWord, 1);
   std::map<march4cpp::IMCObjectName, int> misoByteOffsets = pdoMapMISO.map(1, march4cpp::dataDirection::miso);
 
+
   ASSERT_EQ(0, misoByteOffsets[march4cpp::IMCObjectName::ActualPosition]);
-  ASSERT_EQ(4, misoByteOffsets[march4cpp::IMCObjectName::StatusWord]);
+  ASSERT_EQ(16, misoByteOffsets[march4cpp::IMCObjectName::StatusWord]);
+  ASSERT_EQ(2, misoByteOffsets[march4cpp::IMCObjectName::ControlWord]);
 }
 
-TEST_F(PDOTest, multipleAddObjects)
+TEST_F(PDOTest, invalidRegisters)
 {
   march4cpp::PDOmap pdoMapMISO = march4cpp::PDOmap();
 
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::ActualPosition);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::StatusWord);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::StatusWord);
-  std::map<march4cpp::IMCObjectName, int> misoByteOffsets = pdoMapMISO.map(1, march4cpp::dataDirection::miso);
-  ASSERT_EQ(2, misoByteOffsets.size());
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::ActualPosition,5);
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::StatusWord, -1);
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::ControlWord, 0);
+  std::map<march4cpp::IMCObjectName, int> misoByteOffsets = pdoMapMISO.map(2, march4cpp::dataDirection::miso);
+  ASSERT_EQ(0, misoByteOffsets.size());
 }
 
 TEST_F(PDOTest, ObjectCounts)
 {
     march4cpp::PDOmap pdoMapMISO = march4cpp::PDOmap();
-    pdoMapMISO.addObject(march4cpp::IMCObjectName::CurrentLimit);
-    std::map<march4cpp::IMCObjectName, int> misoByteOffsets = pdoMapMISO.map(1, march4cpp::dataDirection::miso);
+    pdoMapMISO.addObject(march4cpp::IMCObjectName::CurrentLimit, 1);
+    std::map<march4cpp::IMCObjectName, int> misoByteOffsets = pdoMapMISO.map(3, march4cpp::dataDirection::miso);
     ASSERT_EQ(1, misoByteOffsets.count(march4cpp::IMCObjectName::CurrentLimit));
     ASSERT_EQ(0, misoByteOffsets.count(march4cpp::IMCObjectName::DCLinkVoltage));
 }
@@ -45,18 +49,9 @@ TEST_F(PDOTest, ObjectCounts)
 TEST_F(PDOTest, exceedMaxSize)
 {
   march4cpp::PDOmap pdoMapMISO = march4cpp::PDOmap();
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::StatusWord);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::ActualPosition);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::ControlWord);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::DetailedErrorRegister);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::MotionErrorRegister);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::TargetPosition);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::DCLinkVoltage);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::MotorPosition);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::ActualTorque);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::QuickStopDeceleration);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::CurrentLimit);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::DriveTemperature);
-  pdoMapMISO.addObject(march4cpp::IMCObjectName::QuickStopOption);
-  ASSERT_THROW(pdoMapMISO.map(1, march4cpp::dataDirection::miso), std::exception);
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::StatusWord, 1);
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::ActualPosition, 1);
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::ControlWord, 1);
+  pdoMapMISO.addObject(march4cpp::IMCObjectName::DetailedErrorRegister, 1);
+  ASSERT_THROW(pdoMapMISO.map(4, march4cpp::dataDirection::miso), std::exception);
 }
