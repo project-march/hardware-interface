f9da4abbbbefe0790ac9b96df28abb359f0914e2
JorisWeeda <45165354+JorisWeeda@users.noreply.github.com>
2019-12-16 09:47:00 +0100

    added type to the rwc.

--- march_hardware/src/EtherCAT/EthercatSDO.cpp
+++ march_hardware/src/EtherCAT/EthercatSDO.cpp
@@ -18,7 +18,7 @@ namespace march4cpp
 int sdo_bit8(int slave, uint32_t index, uint8_t sub, uint8_t value)
 {
   ROS_DEBUG("sdo_bit8: slaveIndex %i, reg 0x%X, subindex %i, value 0x%X", slave, index, sub, value);
-  ReceivedWorkingCounter = ec_SDOwrite(slave, index, sub, FALSE, 1, &value, EC_TIMEOUTRXM);
+  int ReceivedWorkingCounter = ec_SDOwrite(slave, index, sub, FALSE, 1, &value, EC_TIMEOUTRXM);
   if (ReceivedWorkingCounter == 0)
   {
     ROS_FATAL("Error occurred when writing ID 0x%X to slave %i", slave, index);
@@ -29,7 +29,7 @@ int sdo_bit8(int slave, uint32_t index, uint8_t sub, uint8_t value)
 int sdo_bit16(int slave, uint32_t index, uint8_t sub, uint16_t value)
 {
   ROS_DEBUG("sdo_bit16: slaveIndex %i, reg 0x%X, subindex %i, value 0x%X", slave, index, sub, value);
-  ReceivedWorkingCounter = ec_SDOwrite(slave, index, sub, FALSE, 2, &value, EC_TIMEOUTRXM);
+  int ReceivedWorkingCounter = ec_SDOwrite(slave, index, sub, FALSE, 2, &value, EC_TIMEOUTRXM);
   if (ReceivedWorkingCounter == 0)
   {
     ROS_FATAL("Error occurred when writing ID 0x%X to slave %i", slave, index);
@@ -40,7 +40,7 @@ int sdo_bit16(int slave, uint32_t index, uint8_t sub, uint16_t value)
 int sdo_bit32(int slave, uint32_t index, uint8_t sub, uint32_t value)
 {
   ROS_DEBUG("sdo_bit32: slaveIndex %i, reg 0x%X, subindex %i, value 0x%X", slave, index, sub, value);
-  ReceivedWorkingCounter = ec_SDOwrite(slave, index, sub, FALSE, 4, &value, EC_TIMEOUTRXM);
+  int ReceivedWorkingCounter = ec_SDOwrite(slave, index, sub, FALSE, 4, &value, EC_TIMEOUTRXM);
   if (ReceivedWorkingCounter == 0)
   {
     ROS_FATAL("Error occurred when writing ID 0x%X to slave %i", slave, index);
