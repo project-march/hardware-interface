62457c03ea910114c4c1d320993fc117b1d5500f
jorisweeda <joris.weeda@hotmail.com>
2020-01-02 17:03:17 +0100

    PDO map is configured using the example given in the IMC manual. Also short docstrings have been added to improve readability.

--- march_hardware/include/march_hardware/PDOmap.h
+++ march_hardware/include/march_hardware/PDOmap.h
@@ -13,6 +13,10 @@
 
 namespace march4cpp
 {
+
+/**
+ * Store IMC data as a struct to prevent data overlap
+ */
 struct IMCObject
 {
   int address;  // in IMC memory (see IMC manual)
@@ -29,13 +33,19 @@ struct IMCObject
   }
 };
 
+/**
+ * The data direction to which the PDO is specified is restricted to master in slave out and slave out master in
+ */
 enum class dataDirection
 {
   miso,
   mosi
 };
 
-// If a new object is added to this enum, make sure to also add it to PDOmap::initAllObjects()!
+/**
+ * All the available IMC object names divided over the PDO maps
+ * Note: If a new object is added to this enum, make sure to also add it to PDOmap::initAllObjects()!
+ */
 enum class IMCObjectName
 {
   StatusWord,
@@ -57,24 +67,35 @@ enum class IMCObjectName
 class PDOmap
 {
 public:
-  // Constructor
   PDOmap();
-
-  void addObject(IMCObjectName objectname);
+  void addObject(IMCObjectName objectName);
   std::map<IMCObjectName, int> map(int slaveIndex, dataDirection direction);
 
 private:
+  /**
+ * Initiate all the entered IMC objects to prepare the PDO
+ */
   void initAllObjects();
+
+  /**
+   * This function is used to sort the objects in the allObjects according to data length. This should optimize the
+   * usage of storage
+   */
   void sortPDOObjects();
+
+  /**
+ * Combine the address(hex), sub-index(hex) and length(hex). Example control word: 60400010h
+ */
   uint32_t combineAddressLength(uint16_t address, uint16_t length);
+
   std::map<IMCObjectName, IMCObject> PDOObjects;
   std::map<IMCObjectName, IMCObject> allObjects;
   std::vector<std::pair<IMCObjectName, IMCObject>> sortedPDOObjects;
   std::map<IMCObjectName, int> byteOffsets;
 
-  const int bitsPerReg = 64;
-  const int nrofRegs = 4;
-  const int objectSizes[3] = { 8, 16, 32 };
+  const int bitsPerReg = 64;                 // Maximum amount of bits that  constructed in one PDO message.
+  const int nrofRegs = 4;                    // Amount of registers available.
+  const int objectSizes[3] = { 32, 16, 8 };  // Available sizes.
 };
 }  // namespace march4cpp
 
--- march_hardware/src/PDOmap.cpp
+++ march_hardware/src/PDOmap.cpp
@@ -11,133 +11,6 @@ PDOmap::PDOmap()
   this->initAllObjects();
 }
 
-void PDOmap::addObject(IMCObjectName objectname)
-{
-  if (this->allObjects.count(objectname) != 1)
-  {
-    ROS_WARN("IMC object does not exist (yet), or multiple exist");
-    return;
-  }
-  else if (this->PDOObjects.count(objectname) != 0)
-  {
-    ROS_WARN("IMC object is already added to PDO map");
-    return;
-  }
-  else
-  {
-    this->PDOObjects[objectname] = this->allObjects[objectname];
-  }
-}
-
-std::map<enum IMCObjectName, int> PDOmap::map(int slaveIndex, enum dataDirection direction)
-{
-  this->sortPDOObjects();
-  int reg;
-  int SMAddress;
-  if (direction == dataDirection::miso)
-  {
-    reg = 0x1A00;
-    SMAddress = 0x1C13;
-  }
-  else if (direction == dataDirection::mosi)
-  {
-    reg = 0x1600;
-    SMAddress = 0x1C12;
-  }
-  else
-  {
-    ROS_ERROR("Invalid dataDirection argument");
-  }
-  // Clear SyncManager Object
-  sdo_bit8(slaveIndex, SMAddress, 0, 0);
-  int startReg = reg;
-  int lastFilledReg = reg;
-  int sizeleft = this->bitsPerReg;
-  int counter = 0;
-  int byteOffset = 0;
-  while (this->sortedPDOObjects.size() > 0)
-  {
-    // Check if register is still empty
-    if (sizeleft == this->bitsPerReg)
-    {
-      sdo_bit32(slaveIndex, reg, 0, 0);
-    }
-    // Get next object (from end, because sorted from small to large)
-    std::pair<IMCObjectName, IMCObject> nextObject = this->sortedPDOObjects.back();
-    this->sortedPDOObjects.pop_back();
-    // Add next object to map
-    counter++;
-    sdo_bit32(slaveIndex, reg, counter,
-              this->combineAddressLength(nextObject.second.address, nextObject.second.length));
-    this->byteOffsets[nextObject.first] = byteOffset;
-    byteOffset += nextObject.second.length / 8;
-    sizeleft -= nextObject.second.length;
-    // Check if this was the last object of the list
-    if (this->sortedPDOObjects.size() == 0)
-    {
-      sdo_bit32(slaveIndex, reg, 0, counter);
-      lastFilledReg = reg;
-      reg++;
-    }
-    // else, check if register is full
-    else if (sizeleft <= 0)
-    {
-      sdo_bit32(slaveIndex, reg, 0, counter);
-      reg++;
-      counter = 0;
-      sizeleft = this->bitsPerReg;
-    }
-  }
-  // For the unused registers, set count to zero
-  for (int i = reg; i < startReg + this->nrofRegs; i++)
-  {
-    sdo_bit32(slaveIndex, i, 0, 0);
-  }
-  // For all filled registers, set data to Sync Manager object
-  int count = 0;
-  for (int i = startReg; i <= lastFilledReg; i++)
-  {
-    count++;
-    sdo_bit16(slaveIndex, SMAddress, count, 0x1600);
-  }
-  sdo_bit8(slaveIndex, SMAddress, 0, count);
-  return this->byteOffsets;
-}
-
-void PDOmap::sortPDOObjects()
-{
-  // Sort from small to large
-  int totalbits = 0;
-  for (int i = 0; i < (sizeof(this->objectSizes) / sizeof(this->objectSizes[0])); i++)
-  {
-    std::map<IMCObjectName, IMCObject>::iterator j;
-    for (j = this->PDOObjects.begin(); j != this->PDOObjects.end(); j++)
-    {
-      if (j->second.length == this->objectSizes[i])
-      {
-        std::pair<IMCObjectName, IMCObject> nextObject;
-        nextObject.first = j->first;
-        nextObject.second = j->second;
-        this->sortedPDOObjects.push_back(nextObject);
-        totalbits += this->objectSizes[i];
-      }
-    }
-  }
-  if (totalbits > this->nrofRegs * this->bitsPerReg)
-  {
-    ROS_FATAL("Too many objects in PDO Map (total bits %d, only %d allowed)", totalbits,
-              this->nrofRegs * this->bitsPerReg);
-    throw std::exception();
-  }
-}
-
-uint32_t PDOmap::combineAddressLength(uint16_t address, uint16_t length)
-{
-  uint32_t MSword = ((address & 0xFFFF) << 16);  // Shift 16 bits left for most significant word
-  uint32_t LSword = (length & 0xFFFF);
-  return (MSword | LSword);
-}
-
 void PDOmap::initAllObjects()
 {
   // Object(address, length);
@@ -158,4 +31,129 @@ void PDOmap::initAllObjects()
   // etc...
   // If a new entry is added here, first add it to the enum (in the header file)!
 }
+
+void PDOmap::addObject(IMCObjectName objectName)
+{
+  if (this->allObjects.count(objectName) != 1)
+  {
+    ROS_WARN("IMC object does not exist (yet), or multiple exist");
+    return;
+  }
+  else if (this->PDOObjects.count(objectName) != 0)
+  {
+    ROS_WARN("IMC object is already added to PDO map");
+    return;
+  }
+  else
+  {
+    this->PDOObjects[objectName] = this->allObjects[objectName];
+  }
+}
+
+std::map<enum IMCObjectName, int> PDOmap::map(int slaveIndex, enum dataDirection direction)
+{
+  this->sortPDOObjects();
+  int reg{};
+  int SMAddress{};
+
+  if (direction == dataDirection::miso)
+  {
+    reg = 0x1A00;
+    SMAddress = 0x1C13;
+  }
+  else if (direction == dataDirection::mosi)
+  {
+    reg = 0x1600;
+    SMAddress = 0x1C12;
+  }
+  else
+  {
+    ROS_ERROR("Invalid dataDirection argument");
+  }
+
+  int counter = 1;
+  int currentReg = reg;
+  int byteOffset = 0;
+  int sizeLeft = this->bitsPerReg;
+
+  sdo_bit8(slaveIndex, currentReg, 0, 0);
+  for (const auto& nextObject : sortedPDOObjects)
+  {
+    sizeLeft -= nextObject.second.length;
+    if (byteOffset < 0)
+    {
+      // PDO is filled so it can be enabled again
+      sdo_bit8(slaveIndex, currentReg, 0, counter);
+
+      // Change the sync manager accordingly
+      sdo_bit8(slaveIndex, SMAddress, 0, 0);
+      int currentPDONr = (currentReg - reg) + 1;
+      sdo_bit16(slaveIndex, SMAddress, currentPDONr, currentReg);
+
+      // Move to the next PDO
+      currentReg++;
+      if (currentReg > (reg + nrofRegs))
+      {
+        ROS_ERROR("Amount of registers was overwritten, amount of parameters does not fit in the PDO messages.");
+      }
+
+      sizeLeft = this->bitsPerReg - nextObject.second.length;
+      byteOffset = 0;
+      counter = 1;
+      sdo_bit8(slaveIndex, currentReg, 0, 0);
+    }
+
+    sdo_bit32(slaveIndex, currentReg, counter,
+              this->combineAddressLength(nextObject.second.address, nextObject.second.length));
+    counter++;
+
+    byteOffset += nextObject.second.length / 8;
+    this->byteOffsets[nextObject.first] = byteOffset;
+  }
+
+  currentReg++;
+  if (currentReg <= (reg + nrofRegs))
+  {
+    for (int unusedRegister = reg; unusedRegister < reg + this->nrofRegs; unusedRegister++)
+    {
+      sdo_bit8(slaveIndex, unusedRegister, 0, 0);
+    }
+  }
+
+  return this->byteOffsets;
+}
+
+void PDOmap::sortPDOObjects()
+{
+  int totalBits = 0;
+  for (int i = 0; i < (sizeof(this->objectSizes) / sizeof(this->objectSizes[0])); i++)
+  {
+    std::map<IMCObjectName, IMCObject>::iterator j;
+    for (j = this->PDOObjects.begin(); j != this->PDOObjects.end(); j++)
+    {
+      if (j->second.length == this->objectSizes[i])
+      {
+        std::pair<IMCObjectName, IMCObject> nextObject{};
+        nextObject.first = j->first;
+        nextObject.second = j->second;
+        this->sortedPDOObjects.push_back(nextObject);
+        totalBits += this->objectSizes[i];
+      }
+    }
+  }
+  if (totalBits > this->nrofRegs * this->bitsPerReg)
+  {
+    ROS_FATAL("Too many objects in PDO Map (total bits %d, only %d allowed)", totalBits,
+              this->nrofRegs * this->bitsPerReg);
+    throw std::exception();
+  }
+}
+
+uint32_t PDOmap::combineAddressLength(uint16_t address, uint16_t length)
+{
+  uint32_t MSword = ((address & 0xFFFF) << 16);  // Shift 16 bits left for most significant word
+  uint32_t LSword = (length & 0xFFFF);
+  return (MSword | LSword);
+}
+
 }  // namespace march4cpp
