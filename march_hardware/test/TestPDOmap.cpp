d0f5280bccbbfcc216d1e2f84cb62e43935e4dda
jorisweeda <joris.weeda@hotmail.com>
2020-01-08 17:24:24 +0100

    Removed the test for maximum amount.
    Because it is not possible with the currently defined IMC PDO Objects.

--- march_hardware/test/TestPDOmap.cpp
+++ march_hardware/test/TestPDOmap.cpp
@@ -41,22 +41,3 @@ TEST_F(PDOTest, ObjectCounts)
   ASSERT_EQ(1, misoByteOffsets.count(march4cpp::IMCObjectName::CurrentLimit));
   ASSERT_EQ(0, misoByteOffsets.count(march4cpp::IMCObjectName::DCLinkVoltage));
 }
-
-TEST_F(PDOTest, exceedMaxSize)
-{
-  march4cpp::PDOmap pdoMapMISO = march4cpp::PDOmap();
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::StatusWord);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::ActualPosition);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::ControlWord);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::DetailedErrorRegister);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::MotionErrorRegister);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::TargetPosition);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::DCLinkVoltage);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::MotorPosition);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::ActualTorque);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::QuickStopDeceleration);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::CurrentLimit);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::DriveTemperature);
-  pdoMapMISO.add_object(march4cpp::IMCObjectName::QuickStopOption);
-  ASSERT_THROW(pdoMapMISO.map(1, march4cpp::dataDirection::miso), std::exception);
-}
